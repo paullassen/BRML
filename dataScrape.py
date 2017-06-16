25bcade5e942120cb4297caba6c85e098cb21a4f
Philip Lassen <philiplassen@gmail.com>
2017-06-15 17:05:07 -0700

    Takes battin-logs from players starting in 2009 to the most recent game

--- dataScrape.py
+++ dataScrape.py
@@ -3,17 +3,21 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
-url = 'http://www.baseball-reference.com/players/gl.fcgi?id=troutmi01&t=b&year=2013'
-resultsPage = requests.get(url)
-soup = BeautifulSoup(resultsPage.text, "html5lib")
-table = soup.select_one("#batting_gamelogs")
+year = 2009
 
-headers = [th.text.encode("utf-8") for th in table.select("tr th")]
-
-with open("out.csv", "w") as f:
-    wr = csv.writer(f)
-    wr.writerow(headers)
-    wr.writerows([[td.text.encode("utf-8") for td in row.find_all("td")] for row in table.select("tr + tr")])
+while (year != 2018):
+	url = 'http://www.baseball-reference.com/players/gl.fcgi?id=troutmi01&t=b&year=' + str(year)
+	resultsPage = requests.get(url)
+	soup = BeautifulSoup(resultsPage.text, "html5lib")
+	table = soup.select_one("#batting_gamelogs")
+	year += 1
+	if (table != None):
+		headers = [th.text.encode("utf-8") for th in table.select("tr th")]
+		
+		with open("out3.csv", "a") as f:
+		    wr = csv.writer(f)
+		    wr.writerow(headers)
+		    wr.writerows([[td.text.encode("utf-8") for td in row.find_all("td")] for row in table.select("tr + tr")])
 
 
 
